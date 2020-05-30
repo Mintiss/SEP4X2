d92faad8e4d1b75b9f3f4b92d689c45c7e95ecf0
Mintiss <m.miklasevicius3@gmail.com>
2020-05-30 16:43:29 +0200

    Firebasse update

--- Presentation/app/src/main/java/sep4x2/android/ui/home/HomeFragment.java
+++ Presentation/app/src/main/java/sep4x2/android/ui/home/HomeFragment.java
@@ -1,10 +1,12 @@
 package sep4x2.android.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -21,8 +23,14 @@ import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 import sep4x2.android.R;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class HomeFragment extends Fragment {
 
@@ -43,6 +51,8 @@ public class HomeFragment extends Fragment {
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         homeViewModel.sensorDataClient.updateSensorData();
+
+        
 
         return view;
     }
